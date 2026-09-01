65e8def61811083b138ca609c8aecd308c831747
mahmoudjijawi-hub <mahmoudjijawi@gmail.com>
2026-09-01 23:06:25 +0300

    إعادة ضبط ورفع الملفات من جديد

--- attendance/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from attendance.models import Attendance
-
-
-@admin.register(Attendance)
-class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ("student", "Date", "subject", "Status")
-    list_filter = ("Status", "Date", "subject")
--- grades/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class GradesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "grades"
-    verbose_name = "العلامات"
